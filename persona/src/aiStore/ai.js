c1933078fd694efe9793ce13af2099b45417bdfe
parag jindal <paragjindal2277@gmail.com>
2025-08-15 01:31:05 +0530

    fix issue

--- persona/src/aiStore/ai.js
+++ persona/src/aiStore/ai.js
@@ -6,9 +6,9 @@ export const useAIStore = create((set) => ({
     response: null,
     setResponse: async (systemPrompt,messages) => {
         try {
-            console.log("system prompt - ",systemPrompt,"Messages - ",messages)
+            console.log(`${import.meta.env.BACKEND_URL}/persona-ai`);
             const response = await axios.post(
-              `${import.meta.env.BACKEND_URL}/persona-ai`,
+              `${import.meta.env.VITE_BACKEND_URL}/persona-ai`,
               {
                 systemPrompt,
                 messages,
